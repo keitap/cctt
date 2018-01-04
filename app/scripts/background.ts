034a3a2db872865f0f7d3bd7ff5c9a73967fc883
Keita Kitamura <keitap@gmail.com>
2018-01-05 00:39:01 +0900

    first implementation.

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -1,8 +1,2 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
-
-chrome.runtime.onInstalled.addListener((details) => {
-  console.log('previousVersion', details.previousVersion);
-});
-
-console.log(`'Allo 'Allo! Event Page`);
